14768f1f839d4bccca5a062ec3a3528d1b3cbe42
WithGJR <kevin3372000@gmail.com>
2015-06-04 22:41:51 +0800

    add all lesson contents && make test page more better

--- js/jsx/test.js
+++ js/jsx/test.js
@@ -1,3 +1,65 @@
+var finishedQuestion = [
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+];
+
+var answerCorrectness = [
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+];
+
+var finishedNumber = function() {
+  var num = 0;
+  finishedQuestion.forEach(function(element) {
+    if (element == true) {
+      num += 1;
+    };
+  });
+  return num;
+}
+
+var finishedRate = function() {
+  return (finishedNumber()/14.0)*100.0;
+}
+
+var answerSituation = function() {
+  var total = [0, 0, 0];
+  answerCorrectness.forEach(function(answer) {
+    if (answer == true) {
+      total[0] += 1;
+    }else if (answer == false) {
+      total[1] += 1;
+    }else{
+      total[2] += 1;
+    };
+  });
+  return total;
+}
+
 var Question = React.createClass({displayName: "Question",
   getInitialState: function() {
     return {rightOrNot: null, msg: ""};
@@ -5,10 +67,13 @@ var Question = React.createClass({displayName: "Question",
   handleAnswer: function(event) {
     if(event.target.value == this.props.content.rightAnswer){
       this.setState({rightOrNot: true, msg: "label success"});
+      answerCorrectness[this.props.questionNumber] = true;
     }else{
       this.setState({rightOrNot: false, msg: "label alert"});
+      answerCorrectness[this.props.questionNumber] = false;
     }
-
+    finishedQuestion[this.props.questionNumber] = true;
+    this.props.update();
   },
   render: function() {
     var answerSituationMsg = "";
@@ -23,8 +88,18 @@ var Question = React.createClass({displayName: "Question",
       );
     });
 
+    var style = {
+      width: String(finishedRate())+"%"
+    };
+
+    var numberOfFinishedQuestion = finishedNumber();
+
     return (
       React.createElement("div", {className: "slide"}, 
+        React.createElement("h4", {className: "title"}, "作答比率 ", numberOfFinishedQuestion, "/14"), 
+        React.createElement("div", {className: "progress small-# success radius"}, 
+          React.createElement("span", {className: "meter", style: style})
+        ), 
         React.createElement("h1", {className: "title"}, this.props.content.title), 
         React.createElement("form", {onChange: this.handleAnswer}, 
           React.createElement("div", {className: "center"}, options)
@@ -36,16 +111,43 @@ var Question = React.createClass({displayName: "Question",
 });
 
 var Questions = React.createClass({displayName: "Questions",
+  updateAll: function() {
+    this.forceUpdate();
+  },
   render: function() {
-    var questions = this.props.questions.map(function(question) {
+    var self = this;
+    var questions = this.props.questions.map(function(question, index) {
+      var update = self.updateAll.bind(self)
       return (
-        React.createElement(Question, {content: question})
+        React.createElement(Question, {update: update, content: question, questionNumber: index})
       );
+    });
+    var answerSituations = answerSituation();
+    var styles = answerSituations.map(function(situation){
+      return {width: String((situation/14.0)*100.0)+"%"};
     });
 
     return (
-      React.createElement("div", null, 
-        questions
+      React.createElement("div", {className: "section"}, 
+        questions, 
+
+        React.createElement("div", {className: "slide"}, 
+          React.createElement("h2", {className: "title"}, "答題狀況"), 
+          React.createElement("div", {id: "report"}, 
+            React.createElement("h4", {className: "title"}, "答對 ", answerSituations[0], "題"), 
+            React.createElement("div", {className: "progress small-# success radius"}, 
+              React.createElement("span", {className: "meter", style: styles[0]})
+            ), 
+            React.createElement("h4", {className: "title"}, "答錯 ", answerSituations[1], "題"), 
+            React.createElement("div", {className: "progress small-# alert radius"}, 
+              React.createElement("span", {className: "meter", style: styles[1]})
+            ), 
+            React.createElement("h4", {className: "title"}, "未作答 ", answerSituations[2], "題"), 
+            React.createElement("div", {className: "progress small-# secondary radius"}, 
+              React.createElement("span", {className: "meter", style: styles[2]})
+            )
+          )
+        )
       )
     );
   }
--- jsx_develop/test.jsx
+++ jsx_develop/test.jsx
@@ -1,3 +1,65 @@
+var finishedQuestion = [
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+];
+
+var answerCorrectness = [
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+];
+
+var finishedNumber = function() {
+  var num = 0;
+  finishedQuestion.forEach(function(element) {
+    if (element == true) {
+      num += 1;
+    };
+  });
+  return num;
+}
+
+var finishedRate = function() {
+  return (finishedNumber()/14.0)*100.0;
+}
+
+var answerSituation = function() {
+  var total = [0, 0, 0];
+  answerCorrectness.forEach(function(answer) {
+    if (answer == true) {
+      total[0] += 1;
+    }else if (answer == false) {
+      total[1] += 1;
+    }else{
+      total[2] += 1;
+    };
+  });
+  return total;
+}
+
 var Question = React.createClass({
   getInitialState: function() {
     return {rightOrNot: null, msg: ""};
@@ -5,10 +67,13 @@ var Question = React.createClass({
   handleAnswer: function(event) {
     if(event.target.value == this.props.content.rightAnswer){
       this.setState({rightOrNot: true, msg: "label success"});
+      answerCorrectness[this.props.questionNumber] = true;
     }else{
       this.setState({rightOrNot: false, msg: "label alert"});
+      answerCorrectness[this.props.questionNumber] = false;
     }
-
+    finishedQuestion[this.props.questionNumber] = true;
+    this.props.update();
   },
   render: function() {
     var answerSituationMsg = "";
@@ -23,8 +88,18 @@ var Question = React.createClass({
       );
     });
 
+    var style = {
+      width: String(finishedRate())+"%"
+    };
+
+    var numberOfFinishedQuestion = finishedNumber();
+
     return (
       <div className="slide">
+        <h4 className="title">作答比率 {numberOfFinishedQuestion}/14</h4>
+        <div className="progress small-# success radius">
+          <span className="meter" style={style}></span>
+        </div>
         <h1 className="title">{this.props.content.title}</h1>
         <form onChange={this.handleAnswer}>
           <div className="center">{options}</div>
@@ -36,16 +111,43 @@ var Question = React.createClass({
 });
 
 var Questions = React.createClass({
+  updateAll: function() {
+    this.forceUpdate();
+  },
   render: function() {
-    var questions = this.props.questions.map(function(question) {
+    var self = this;
+    var questions = this.props.questions.map(function(question, index) {
+      var update = self.updateAll.bind(self)
       return (
-        <Question content={question} />
+        <Question update={update} content={question} questionNumber={index} />
       );
+    });
+    var answerSituations = answerSituation();
+    var styles = answerSituations.map(function(situation){
+      return {width: String((situation/14.0)*100.0)+"%"};
     });
 
     return (
-      <div>
+      <div className="section">
         {questions}
+
+        <div className="slide">
+          <h2 className="title">答題狀況</h2>
+          <div id="report">
+            <h4 className="title">答對 {answerSituations[0]}題</h4>
+            <div className="progress small-# success radius">
+              <span className="meter" style={styles[0]}></span>
+            </div>
+            <h4 className="title">答錯 {answerSituations[1]}題</h4>
+            <div className="progress small-# alert radius">
+              <span className="meter" style={styles[1]}></span>
+            </div>
+            <h4 className="title">未作答 {answerSituations[2]}題</h4>
+            <div className="progress small-# secondary radius">
+              <span className="meter" style={styles[2]}></span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
